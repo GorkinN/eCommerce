3cf009130db0116f121ce44087e78a7606173b5f
Cahta911 <66055350+Cahta911@users.noreply.github.com>
2021-11-18 22:30:13 +0500

    made JS for categories buttons

---
+++ js/categoriesOpen.js
@@ -0,0 +1,35 @@
+var categoriesButton = document.querySelectorAll('.section__menu-button');
+var menuList = document.querySelectorAll('.section__menu-list');
+
+var openMenu = function (currentButton, currentMenu) {
+    currentButton.addEventListener('click', function() {
+       let listItems = currentMenu.querySelectorAll('.section__menu-list-item');
+       // при помощи костыля в виде класса toggle меняем надпись на кнопке =(
+        // через некоторое время сделаю нормально.
+        // Надо глянуть как пересчитать объекты в коллекции, иначе так и придётся
+        // костылить верстку пустыми классами
+       if (currentButton.classList.contains('toggle')) {
+           for (let listItem of listItems) {
+               if (listItem.classList.contains('toggle')) {
+                   listItem.classList.add('hidden');
+                   listItem.classList.remove('toggle');
+               }
+           }
+           currentButton.textContent='More categories';
+       } else {
+           for (let listItem of listItems) {
+               if (listItem.classList.contains('hidden')) {
+                   listItem.classList.remove('hidden');
+                   listItem.classList.add('toggle');
+               }
+           }
+           currentButton.textContent='Less categories';
+       }
+        currentButton.classList.toggle('toggle');
+    });
+};
+
+//раздаём полномочия каждой кнопке для своего меню
+for (let i = 0 ; i <= categoriesButton.length - 1 ; i++) {
+    openMenu(categoriesButton[i], menuList[i]);
+}
