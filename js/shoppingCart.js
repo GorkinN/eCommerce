68da261bc9ea525dd1abf9f9c5286f68c527e8c8
Cahta911 <66055350+Cahta911@users.noreply.github.com>
2021-12-28 15:06:17 +0500

    fixes

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,5 +1,5 @@
-let openCartButton = document.getElementById("shopping-cart-open-button")
 let shoppingCartWindow = document.querySelector(".shopping-cart-window");
+let openCartButton = document.getElementById("shopping-cart-open-button")
 let closeCartButton = shoppingCartWindow.querySelector(".shopping-cart-window__close-button");
 
 openCartButton.addEventListener("click", function() {
